40bd9290c6229642cee48cdfb3dc16d46070a976
ksekmen1 <kaya.sekmen@gmail.com>
2023-09-21 14:47:35 +0200

    Add project files.

---
+++ IMDBDataImporter/NormalInserter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBDataImporter
+{
+    public class NormalInserter
+    {
+        private string ConnString = "server=localhost,database=IMDB"+"user id=sa, " +
+            "password=testkode,TrustServerCertificate=True";
+
+        public void InsertData(List<Title> titles)
+        {
+            SqlConnection sqlConn= new SqlConnection(ConnString);
+            sqlConn.Open();
+
+            foreach(Title title in titles)
+            {
+                SqlCommand sqlCommand= new SqlCommand(
+                    "" +
+                    "INSERT INTO [dbo].[Titles]" +
+                    "([tconst],[titleType],[primaryTitle],[originalTitle]" +
+                    "[isAdult],[startYear],[endYear],[runTimeMinutes])" +
+                    "VALUES" +
+                    $"('{title.tconst}','{title.titleType}'," +
+                    $"('{title.primaryTitle}','{title.originalTitle}'," +
+                    $"({title.isAdult},{title.startYear}," +
+                    $"({title.endYear},{title.runTimeMinutes})," 
+                    , sqlConn);
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                }catch(Exception ex)
+                {
+                    Console.WriteLine(sqlCommand.CommandText);
+                }
+                
+            }
+        }
+    }
+}
---
+++ IMDBDataImporter/Program.cs
@@ -0,0 +1,45 @@
+﻿using IMDBDataImporter;
+
+List<Title> titles = new List<Title>();
+
+foreach (string line in System.IO.File.ReadLines(@"C:\Users\Kaya\Desktop\data\data.tsv").Skip(1).Take(1000))
+{
+    string[] values = line.Split("\t");
+    if (values.Length == 9)
+    {
+        titles.Add(new Title(
+            values[0], values[1], values[2], values[3],
+            ConvertToBool(values[4]), ConvertToInt(values[5]),
+            ConvertToInt(values[6]),
+            ConvertToInt(values[7])
+            ));
+    }
+}
+
+Console.WriteLine(titles.Count);
+NormalInserter myInserter= new NormalInserter();
+myInserter.InsertData(titles);
+
+bool ConvertToBool(string input)
+{
+    if(input == "0")
+    {
+        return false;
+    } else if (input == "1")
+    {
+        return true;
+    }
+    throw new ArgumentException("Kolonne er ikke 01 eller 1, men" + input);
+}
+
+int? ConvertToInt(string input)
+{
+    if (input.ToLower() == @"/n")
+    {
+        return null;
+    }
+    else
+    {
+        return int.Parse(input);
+    }
+}
